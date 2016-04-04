1a88672a8b60fdd798097d1e75e1a18efcb4c906
rhexo <mmusolov@gmail.com>
2016-04-04 22:10:26 +0400

    bitset changes

--- bitset.cpp
+++ bitset.cpp
@@ -12,16 +12,22 @@ namespace mp {
 
 
   bitset::bitset() {
-    data = nullptr;
-    length = 0;
+    this->free();
+  };
+
+
+  bitset::bitset(bitset& b){
+    this->copy(b.data,b.length);    
+  };
+
+  bitset::bitset(bitset&& b){
+    this->copy(b.data,b.length);    
   };
 
   bitset::~bitset() {
-    if (data != nullptr)
-      delete data;
-    length = 0;
+    this->free();
   };
-
+    
   /** Получаем битовое число из строки */
   void 
   bitset::from_char(NumSys q, char* str) {
@@ -60,8 +66,9 @@ namespace mp {
     // Определяем кластер бита
     __MP_INDEX x = index / ( sizeof(__MP_TYPE) * byte_length );
     __MP_INDEX y = index % ( sizeof(__MP_TYPE) * byte_length );
-
-    return (data[x] >> (y-1)) & 1;
+    
+    // get: bit(y) of cluster(x)
+    return (data[x] >> y) & 1;
 
   };
   
@@ -79,11 +86,11 @@ namespace mp {
     __MP_INDEX y = index % ( sizeof(__MP_TYPE) * byte_length );
 
     if (b & 1) {
-      // y-1 -> 1
-      data[x] |= 1 << (y-1);
+      // bit(y) of cluster(x) -> 1
+      data[x] |= 1 << y;
     } else {
-      // y-1 -> 0
-      data[x] &= ~(1 << (y-1));
+      // bit(y) of cluster(x) -> 0
+      data[x] &= ~(1 << y);
     }    
     
   };
@@ -92,6 +99,34 @@ namespace mp {
   __MP_INDEX
   bitset::count() const {
     return (__MP_INDEX)( length * sizeof(__MP_TYPE) * byte_length );
+  };
+  
+  /** Очищаем сегмент памяти, используемый для хранения числа
+   */
+  void
+  bitset::free() {
+    if (data != nullptr)
+      delete data;
+    length = 0;
+  }
+  
+  /** Копируем представление числа из области памяти
+   */
+  void 
+  bitset::copy(__MP_TYPE* __data,__MP_INDEX __length){
+
+    this->free();
+
+    if (__data != nullptr) {
+      // Выделяем область памяти под хранение числа
+      data = new __MP_TYPE[__length];
+      // Воссоздаем из объекта по ссылке
+      for (int i=0;i<__length;i++) {
+        data[i] = __data[i]; 
+      }
+      length = __length;
+    }
+
   };
 
   /** Проверяем, что индекс является корректным и указывает 
@@ -108,6 +143,24 @@ namespace mp {
   // Вывод: "1001" -> 0b1001
   void bitset::char_to_bin_deduce(char *str) {
 
+    __MP_INDEX i = 0;
+    while (str[i] != '\0') {
+      // Допускается синтаксис: как 1001, так и 0b1001.
+      if (i==1 && str[i] == 'b')
+        continue;
+      if (str[i] == '1') {
+        set(i,0b1);
+      } else {
+        if (str[i] == '0') {
+          set(i,0b0);
+        } else {
+          throw std::runtime_error(std::string(1,str[i]) + 
+                                   std::string(" не относится к двоичной системе счисления."));
+        }      
+      }
+      ++i;
+    }
+
   };
 
   // Вывод: "9" -> 0b1001
@@ -115,4 +168,30 @@ namespace mp {
 
   };
 
+  bitset sum(bitset& a, bitset& b){
+    
+    return bitset();
+  };
+  
+  
+  namespace core {
+    
+    /** Раздел центральных функций по раоте с битами */
+
+    bit_t 
+    get_majority_vector(__MP_TYPE __a, __MP_TYPE __b, __MP_TYPE& __c){
+      // c(x,y,z) = (x&y)|(y&z)|(x&z)
+
+      // выполняем предвычисление вектора переносов - __c
+      
+    };
+
+    __MP_TYPE 
+    get_parity_vector(__MP_TYPE __a, __MP_TYPE __b, __MP_TYPE __c) {
+      // s(x,y,z) = x^y^z
+    };
+
+
+  };
+  
 };
--- include/bitset.hpp
+++ include/bitset.hpp
@@ -16,10 +16,12 @@ namespace mp {
 #define __MP_TYPE ui32_t
 #define __MP_INDEX ui32_t
 
+
   static const int byte_length = 8;
 
   //static constexpr int bitarity = 64;
   
+
   /**
      bits operations 
      setting bit(x): number |= 1 << x;
@@ -38,25 +40,88 @@ namespace mp {
     
     bitset();
     ~bitset();
+    // copy
+    bitset(bitset&);
+    bitset(bitset&&);
 
     __MP_TYPE *data;
     __MP_INDEX length;
 
-    bit_t get(__MP_INDEX);
-    void set(__MP_INDEX, bit_t);
-    __MP_TYPE count() const;
+    bit_t 
+    get(__MP_INDEX);
+
+    void 
+    set(__MP_INDEX, bit_t);
+
+    __MP_TYPE 
+    count() const;
     
-    void from_char(NumSys,char*);
-    char* to_char(NumSys) const;
-    
+    void 
+    from_char(NumSys,char*);
+
+    char* 
+    to_char(NumSys) const;        
+
   private: 
-    void check_index_valid(__MP_INDEX);
+    
+    //Очищаем память выделенную под хранение числа
+    void 
+    free();
+    // Копируем из области памяти
+    void 
+    copy(__MP_TYPE*,__MP_INDEX);
+    
+    // Проверяем, что индекс, определенный как параметр, соответствует всем 
+    // ограничениям, чтобы называться таковым
+    void 
+    check_index_valid(__MP_INDEX);
     // Вывод: "1001" -> 0b1001
-    void char_to_bin_deduce(char *);
+    void 
+    char_to_bin_deduce(char *);
     // Вывод: "9" -> 0b1001
-    void char_to_dec_deduce(char *);
+    void 
+    char_to_dec_deduce(char *);
   };
 
+
+  /** Операция: Сложение
+   *  Реализуем по схеме сложения с предвычислением переносов
+   */
+  bitset 
+  sum(bitset&, bitset&);
+  
+  namespace core {
+
+    /** Область центральных функция для реализации операций над типом bitset */
+
+    // допустимые коммутации между переносами
+    // SH_K - kill; i-ый бит не зависит от типа переноса i-1 бита
+    // SH_P - propagate; i-ый бит зависит от типа переноса i-1 бита
+    // SH_G - generate; i-ый бит генерирует перенос в i+1 
+    enum shift_type {SH_K = 0, SH_P = 1, SH_G = -1 };
+  
+    // Коммутационная таблица по типам переносов
+    // shift_type[i+1] = SH_TAB[shift_type[i-1]][shift_type[i]] - рекурентная форма
+    const int SH_TAB[3][3] = { {SH_K, SH_K, SH_G},
+                               {SH_K, SH_P, SH_G},
+                               {SH_K, SH_G, SH_G} };
+
+    /** Вычисляем вектор переносов для типа __MP_TYPE 
+     *  В качестве результата возвращаем бит переноса 
+     *  (length * sizeof( __MP_TYPE )) + 1, т.е. ||__MP_TYPE||+1 бит.
+     */
+    bit_t 
+    get_majority_vector(__MP_TYPE __a, __MP_TYPE __b, __MP_TYPE& __c);
+
+    /** Вычисляем четность (parity).
+     *  Результатом является вектор битов по основанию __MP_TYPE.
+     *  Если в прошлом (на i-1 шаге) был вычислен бит переноса, он должен быть учтен в 
+     *  векторе __c.
+     */
+    __MP_TYPE 
+    get_parity_vector(__MP_TYPE __a, __MP_TYPE __b, __MP_TYPE __c);
+    
+  }
 };
 
 #endif
